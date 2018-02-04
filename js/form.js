2ebc4b34c6ceebf066edce4845f5153de30b6b2c
Václav Pelíšek <peldax@gmail.com>
2018-02-04 18:55:51 +0100

    Update form.js

--- js/form.js
+++ js/form.js
@@ -66,6 +66,21 @@ refreshPlugins = function (el)
 
     $(el).find('[data-dependentselectbox]').dependentSelectBox();
     
+    $(el).find('input.date, input.datetime-local').each(function(i, el) {
+        el = $(el);
+        el.get(0).type = 'text';
+        el.datetimepicker({
+            startDate: el.attr('min'),
+            endDate: el.attr('max'),
+            weekStart: 1,
+            language: el.data('language'),
+            minView: el.is('.date') ? 'month' : 'hour',
+            format: el.is('.date') ? 'd. m. yyyy' : 'd. m. yyyy - hh:ii', // for seconds support use 'd. m. yyyy - hh:ii:ss'
+            autoclose: true
+        });
+        el.attr('value') && el.datetimepicker('setValue');
+    });
+    
     if ($('.g-recaptcha').length !== 0)
     {
         g_ReCaptchaOnLoad();
